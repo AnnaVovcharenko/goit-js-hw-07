0c1573079c0703ea1749a0e74c79b39e2ef480eb
Vovcharenko Anna <129333985+AnnaVovcharenko@users.noreply.github.com>
2023-08-06 15:58:54 +0300

    hw-7 06

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -7,59 +7,26 @@ function createMrkup(arr) {
       <img class="gallery__image" src="${preview}" alt="${description}" data-source="${original}"width="340" height="300"/>
     </a>
   </li>`).join('')
-
 }
-
 container.insertAdjacentHTML('beforeend', createMrkup(galleryItems))
 container.addEventListener('click', handletGalleryClick)
-
 function handletGalleryClick(evt) {
     evt.preventDefault();
     if (!evt.target.classList.contains('gallery__image')) {
         return;
     }
     const picture = evt.target.dataset.source;
-    const instance = basicLightbox.create(`
-    <div class = "modal"> <img src="${picture}" alt=""/></div>`)
-     const modalImage = instance.element().querySelector('img');
+    const instance = basicLightbox.create(`<img src="${picture}" alt=""/>`,{
+        onShow: (instance) => {
+          container.addEventListener('keydown', onEscPress );
+        },
+        onClose: (instance) => {
+        container.removeEventListener('keydown',onEscPress );
+        },
+      }
+    );   
     instance.show();
-      modalImage.addEventListener('click', () => {
-        instance.close();
-    });
-    container.addEventListener('keydown', (evt) => {
-        if (evt.code === "Escape") {
-            instance.close();
-        }
-    });
+function  onEscPress (evt) {
+  if (evt.code === "Escape") {
+    instance.close();}};
 }
-
-
-//{
-        // onShow: (instance) => {
-        //    instance.show();
-        // },
-        // onClose: (instance) => {
-        //     instance.close();
-        // },
-        // }
-        // );
-
-        //function handletGalleryClick(evt) {
-         //   evt.preventDefault();
-           // if (!evt.target.classList.contains('gallery__image')) {
-          ///      return;
-           // }
-          //  console.log(evt.target);
-          //  const picture = evt.target.dataset.source;
-           // const instance = basicLightbox.create(`
-           // <div class = "modal"> <img src="${picture}" alt=""/></div>`)
-           // instance.show();
-          
-           // container.addEventListener('keydown', (evt) => {
-          //      if (evt.code === "Escape") {
-          //          instance.close();
-          //      }
-          //  });
-       // }
-
-
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -7,21 +7,9 @@ function createMrkup(arr) {
       <img class="gallery__image" src="${preview}" alt="${description}"/>
     </a>
   </li>`).join('')
-
 }
 container.insertAdjacentHTML('beforeend', createMrkup(galleryItems))
 let gallery = new SimpleLightbox(".gallery a", { captionsData: "alt", captionDelay: 250 });
 
 
-
-//var lightbox = new SimpleLightbox('.gallery a', { 
-//captionData: 'alt',
- //captionDelay: 250
-//});
-
-//let gallery = new SimpleLightbox('.gallery a');
-//gallery.on('shown.simplelightbox', function () {
-//captionData: 'alt';
-//captionDelay: 250;
-//});
 
